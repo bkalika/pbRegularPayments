4f766cc6bbd3682908f25e53c0e8e9070ef9be27
bkalika <bogdan.kalika@gmail.com>
2022-12-18 22:27:13 +0200

    Added INSERT test values in the DB.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,7 +22,6 @@ CREATE TABLE IF NOT EXISTS receiver(
 );
 CREATE SEQUENCE IF NOT EXISTS receiver_sequence START 1000 INCREMENT 1;
 
-
 CREATE TABLE IF NOT EXISTS payment (
     id bigserial primary key,
     card_id bigserial references card(id),
@@ -40,3 +39,26 @@ CREATE TABLE IF NOT EXISTS journal (
     status varchar(10)
 );
 CREATE SEQUENCE IF NOT EXISTS journal_sequence START 1000 INCREMENT 1;
+
+-- BEGIN;
+--     INSERT INTO sender(first_name, last_name, inn) VALUES (
+--         'First sender first name', 'First sender last name', 565216791);
+--     INSERT INTO sender(first_name, last_name, inn) VALUES (
+--         'Second sender first name', 'Second sender last name', 565216792);
+--     INSERT INTO card(owner_id, number) VALUES (
+--         1, 4567234512348920);
+--     INSERT INTO card(owner_id, number) VALUES (
+--         2, 4567234512348955);
+--     INSERT INTO receiver(iban, mfo, okpo, name) VALUES (
+--         'UA123456789012345678901234091', 555554, 878701, 'First receiver');
+--     INSERT INTO receiver(iban, mfo, okpo, name) VALUES (
+--        'UA123456789012349678901234092', 555534, 878001, 'Second receiver');
+--     INSERT INTO receiver(iban, mfo, okpo, name) VALUES (
+--        'UA123416789012349678901234082', 550534, 808001, 'Third receiver');
+--     INSERT INTO payment(card_id, receiver_id, period, amount) VALUES (
+--         1, 1, 'MINUTE', 1000);
+--     INSERT INTO payment(card_id, receiver_id, period, amount) VALUES (
+--         2, 2, 'HOUR', 2000);
+--     INSERT INTO payment(card_id, receiver_id, period, amount) VALUES (
+--         2, 3, 'DAY', 3000);
+-- COMMIT;
